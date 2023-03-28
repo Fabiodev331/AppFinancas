2e8a2fb7bb53580c755cbaa370fa5f1dfb5e9d46
devprogramador <sjosefabio1996@gmail.com>
2023-03-28 12:40:22 -0300

    iniciando appfinancas

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Text, View, } from 'react-native';
+
+import {Container, Title} from './src/styles';
+
+export default function App() {
+
+  return(
+    <Container>
+      <Title>Dev Programador</Title>
+    </Container>
+  );
+}
+
