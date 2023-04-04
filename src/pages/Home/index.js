858ee37e7f688177cb0334594092ea3c6db9f4a3
devprogramador <sjosefabio1996@gmail.com>
2023-04-04 13:33:09 -0300

    criando os cards da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,58 @@
-import React, {useContext} from "react";
+import React, {useContext, useState, useEffect} from "react";
 import { Text, View, Button } from "react-native";
 
 import { AuthContext } from "../../contexts/auth";
 
 import Header from "../../components/Header";
-import { Background } from "./styles";
+import { Background, ListBalance } from "./styles";
+import api from "../../services/api";
+import { format } from "date-fns";
+
+import { useIsFocused } from "@react-navigation/native";
 
 export default function Home(){
+    const isFocused = useIsFocused();
+
+    const [listBalance, setListBalance] = useState([]);
+    const [dateMoviments, setDateMoviments] = useState(new Date());
+
+    useEffect(()=> {
+        let isActive = true;
+
+        async function getMoviments(){
+            let dateFormated = format(dateMoviments, "dd/MM/yyyy");
+
+            const balance = await api.get('/balance', {
+                params:{
+                    date: dateFormated
+                }
+            })
+
+            if(isActive){
+                setListBalance(balance.data);
+            }
+        }
+
+        getMoviments();
+
+        return () => isActive = false;
+
+    }, [isFocused])
+
     const { signOut, user } = useContext(AuthContext);
     return(
         <Background>
-            <Header title="Minhas movimentações" />
+            <Header 
+                title="Minhas movimentações" 
+            />
+
+            <ListBalance 
+                data={listBalance}
+                horizontal={true}
+                showHorizontalScrollIndicator={false}
+                keyExtractor={item => item.tag }
+               
+            />
         </Background>
     );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,3 +4,7 @@ export const Background = styled.SafeAreaView`
 flex:1;
 background-color: #dfe8ff;
 `;
+
+export const ListBalance = styled.FlatList`
+max-height: 190px;
+`;
